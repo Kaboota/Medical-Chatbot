3b6691c429f15cf1b55b3670840deb61f9f8719e
Kavita Chauhan <kavitachauhan0212@gmail.com>
2026-08-21 15:55:13 +0530

    Update requirements and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Kavita Chauhan",
+    author_email="kavitachauhan0212@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
